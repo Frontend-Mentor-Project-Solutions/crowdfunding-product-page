d9f6ec32a7cc11d539b7a41a4d63390cdcfba02d
Kalil Subaan Buraaleh <91347702+kalilsub@users.noreply.github.com>
2026-07-17 00:59:47 +0200

    implement basic vertical spacing between elements

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,14 @@ import Header from "./components/Header.vue"
     <Header />
 
     <main>
-      <section class="project-info card">
+      <section class="section project-info card">
         <h1>Mastercraft Bamboo Monitor Riser</h1>
         <p>A beautifully handcrafted monitor stand to reduce neck and eye strain.</p>
         <button>Back this project</button>
         <button>Bookmark</button>
       </section>
 
-      <section class="funding-info card">
+      <section class="section funding-info card">
         <ul role="list" class="funding-stats">
           <li><span>$89,914</span> <span>of $100,000 backed</span></li>
           <li><span>5,007</span> <span>total backers</span></li>
@@ -25,7 +25,7 @@ import Header from "./components/Header.vue"
       </section>
 
       <div class="card">
-        <section>
+        <section class="section">
           <h2>About this project</h2>
           <p>
             The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your
@@ -33,15 +33,15 @@ import Header from "./components/Header.vue"
             potential to improve your posture and make you more comfortable while at work, helping
             you stay focused on the task at hand.
           </p>
+        </section>
+
+        <section class="section">
+          <h2 hidden>Rewards</h2>
 
           <p>
             Featuring artisan craftsmanship, the simplicity of design creates extra desk space below
             your computer to allow notepads, pens, and USB sticks to be stored under the stand.
           </p>
-        </section>
-
-        <section>
-          <h2>Rewards</h2>
 
           <ul role="list"></ul>
         </section>
