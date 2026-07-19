8e0bc1cdca9c9dbaad454bf3e68656188d6fda63
Kalil Subaan Buraaleh <91347702+kalilsub@users.noreply.github.com>
2026-07-19 15:17:27 +0200

    implement header layout: overlap main onto header background image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,11 @@ import AppLogo from "./icons/AppLogo.vue"
 
 <template>
   <header class="full-width">
-    <div class="breakout">
+    <div class="breakout header-inner">
       <AppLogo />
 
       <nav aria-label="Main navigation">
-        <ul role="list">
+        <ul role="list" class="nav-list">
           <li><a href="#">About</a></li>
           <li><a href="#">Discover</a></li>
           <li><a href="#">Get Started</a></li>
@@ -21,6 +21,19 @@ import AppLogo from "./icons/AppLogo.vue"
 <style scoped>
 header {
   background-image: url("../assets/images/image-hero-desktop.jpg");
+  min-height: 400px;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
+
+.header-inner {
+  padding-block-start: clamp(2rem, 0.333rem + 5.556vw, 3rem);
+}
+
+.nav-list {
+  display: flex;
+  gap: 2rem;
 }
 
 header > * {
